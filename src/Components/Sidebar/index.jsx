5ee1512bec521a6c109dda8bb9d030daf2f29212
Adrián Martínez <adrianmcmtz11@outlook.com>
2023-06-14 18:00:02 -0600

    Posicionando sidebar de dashboard

--- src/Components/Sidebar/index.jsx
+++ src/Components/Sidebar/index.jsx
@@ -1,6 +1,6 @@
 function Sidebar() {
     return(
-        <section className="w-[28vw] h-[max-content] min-h-[100vh] bg-[#F6FAFF]">
+        <section className="fixed top-0 right-0 w-[28vw] h-[max-content] min-h-[100vh] bg-[#F6FAFF]">
             <div>
                 <h1>Titulo</h1>
             </div>
