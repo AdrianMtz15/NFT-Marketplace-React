02ef2550cffb5125e7b441cfa3fb82fe9dda4b2a
Adrián Martínez <adrianmcmtz11@outlook.com>
2023-06-20 15:30:38 -0600

    Creación de componente ProfileBar e instalando heroicons

--- src/Components/Sidebar/index.jsx
+++ src/Components/Sidebar/index.jsx
@@ -1,12 +1,12 @@
+import { ProfileBar } from "../ProfileBar"
+
 function Sidebar() {
     return(
-        <section className="fixed top-0 right-0 w-[30vw] h-[max-content] min-h-[100vh] bg-[#F6FAFF]">
-            <div>
-                <h1>Titulo</h1>
-            </div>
+        <aside className="fixed top-0 right-0 w-[30vw] h-[max-content] min-h-[100vh] bg-[#F6FAFF]">
+            <ProfileBar/>
             <div></div>
             <div></div>
-        </section>
+        </aside>
     )
 }
 
--- src/Pages/Cart/index.jsx
+++ src/Pages/Cart/index.jsx
@@ -7,12 +7,14 @@ function Cart() {
   const itemsContext = useContext(ItemContext);
 
   return (
-    <div className="relative grid gap-[2rem] grid-cols-3 w-full h-max-h">
-      {
-        itemsContext.items?.map((item) => (
-          item.isInCart ? <Card key={item.id} item={item}/> : null
-        ))
-      }
+    <div className="relative p-[2.5%] ml-[20vw] w-[50vw]">
+      <div className="relative grid gap-[2rem] grid-cols-3 w-full h-max-h">
+        {
+          itemsContext.items?.map((item) => (
+            item.isInCart ? <Card key={item.id} item={item}/> : null
+          ))
+        }
+      </div>
     </div>
   )
 }
