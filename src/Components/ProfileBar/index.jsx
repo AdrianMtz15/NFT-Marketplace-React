0cd80a47e5167134db855a9955741ca1b5932a74
Adrián Martínez <adrianmcmtz11@outlook.com>
2023-07-08 00:45:13 -0600

    Creando form para cambiar datos de usuario

--- src/Components/ProfileBar/index.jsx
+++ src/Components/ProfileBar/index.jsx
@@ -1,6 +1,13 @@
+import { useContext } from 'react';
+import { ItemContext } from '../../Context';
+
 import { BellIcon, ChevronDownIcon } from '@heroicons/react/24/solid'
 
 function ProfileBar() {
+  const {
+    user
+  } = useContext(ItemContext);
+
   return(
     <div className="shadow border-solid border-[#D9D9D9] border-l 
       z-10 h-[10vh] w-[30vw] bg-white flex flex-row justify-center 
@@ -9,11 +16,19 @@ function ProfileBar() {
       <BellIcon className="w-[2rem] h-[2rem] mx-[8px] cursor-pointer"/>
 
       <div className="mx-[8px] flex flex-row items-center justify-center cursor-pointer">
+
         <img 
           className="mx-[8px] w-[2.5vw]" 
           src="https://firebasestorage.googleapis.com/v0/b/nft-marketplace-94b84.appspot.com/o/users-photos%2FEllipse%204.png?alt=media&token=853964c0-4bf2-4e58-9c60-e17a08448003" 
-          alt="user-photo" />
-        <p className="text-[1.6rem] font-medium text-[#4D4A4A] font-sans mr-[5px]">Doadesignz</p>
+          alt="user-photo" 
+        />
+
+        <p 
+          className="text-[1.6rem] font-medium text-[#4D4A4A] 
+          font-sans mr-[5px]">
+            {user?.name}
+        </p>
+        
         <ChevronDownIcon className="w-[2rem] h-[2rem]"/>
       </div>
     </div>
